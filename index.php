8304d323fb853e283d8c057f0fc19c97c461bd6f
Gubbydub <szhiglinskiy@gmail.com>
2022-11-08 03:50:31 +0500

    test 4

--- index.php
+++ index.php
@@ -2,3 +2,10 @@
 
 //regular expressions
 //preeg_muth, preg_replace
+
+$string = 'Ученик сидит за партой.';
+$pattern = '/Ученик/';
+
+$result = preg_match($pattern, $string);
+
+var_dump($result);
